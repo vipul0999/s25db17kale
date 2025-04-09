5004feedebfe87a9f6e7f0e6ddee7be930b1030b
vipul0999 <kalevipul12@gmail.com>
2025-04-08 22:37:02 -0500

    gadgets file is updated

--- controllers/gadget.js
+++ controllers/gadget.js
@@ -1,7 +1,7 @@
 // controllers/gadget.js
 const Gadget = require('../models/gadget');
 
-// List of all Gadgets
+// List of all Gadgets as JSON
 exports.gadget_list = async function (req, res) {
   try {
     const gadgets = await Gadget.find();
@@ -11,14 +11,36 @@ exports.gadget_list = async function (req, res) {
   }
 };
 
+// View to display all Gadgets using Pug
+exports.gadget_view_all_Page = async function (req, res) {
+  try {
+    const gadgets = await Gadget.find();
+    res.render('gadgets', { title: 'Gadget Search Results', results: gadgets });
+  } catch (err) {
+    res.status(500);
+    res.send(`{"error": "${err}"}`);
+  }
+};
+
 // Details for a specific Gadget
 exports.gadget_detail = function (req, res) {
   res.send('NOT IMPLEMENTED: Gadget detail: ' + req.params.id);
 };
 
 // Handle Gadget create on POST
-exports.gadget_create_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Gadget create POST');
+exports.gadget_create_post = async function (req, res) {
+  const gadget = new Gadget({
+    gadget_name: req.body.gadget_name,
+    brand: req.body.brand,
+    price: req.body.price
+  });
+
+  try {
+    const result = await gadget.save();
+    res.json(result);
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
 };
 
 // Handle Gadget delete on DELETE
