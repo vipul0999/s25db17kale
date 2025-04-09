8b07d5c80df38aba13bfb9fbf5dc248df79d6945
vipul0999 <kalevipul12@gmail.com>
2025-04-09 14:39:44 -0500

    Removed duplicate createError declaration

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// app.js
 require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
@@ -45,63 +44,40 @@ async function recreateDB() {
   console.log('📦 Sample gadgets saved');
 }
 
-let reseed = true;
+const reseed = true;
 if (reseed) recreateDB();
-
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var gridRouter = require('./routes/grid');
-var pickRouter = require('./routes/pick');
-var gadgetsRouter = require('./routes/gadgets'); // Add this line
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define the routes
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/grid', gridRouter);
 app.use('/', pickRouter);
-
 app.use('/gadgets', gadgetsRouter);
 app.use('/resource', resourceRouter);
-app.use('/gadgets', gadgetsRouter); // Add this line
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-
 module.exports = app;
-
